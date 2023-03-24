46db50ca8db2b77ae8e3612a225b724c32a18b7a
mauroorru3 <mauroorru3@gmail.com>
2023-03-24 13:04:06 +0100

    added chain, dockerfile and ibc module

---
+++ x/universitychainit/genesis_test.go
@@ -0,0 +1,31 @@
+package universitychainit_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "university_chain_it/testutil/keeper"
+	"university_chain_it/testutil/nullify"
+	"university_chain_it/x/universitychainit"
+	"university_chain_it/x/universitychainit/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+		PortId: types.PortID,
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.UniversitychainitKeeper(t)
+	universitychainit.InitGenesis(ctx, *k, genesisState)
+	got := universitychainit.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	require.Equal(t, genesisState.PortId, got.PortId)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
